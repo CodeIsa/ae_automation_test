52c2d4250738636e07fca18a402b428f0766d169
Isabelle <48109634+CodeIsa@users.noreply.github.com>
2023-08-22 21:59:27 -0300

    Update commandsMenu.js

--- cypress/support/commandsMenu.js
+++ cypress/support/commandsMenu.js
@@ -5,6 +5,8 @@ Cypress.Commands.add("openMenu", () => {
 
 Cypress.Commands.add("clickMenuOption", (menuOption) => {
   cy.get("#nav-main").contains("Todos").click({ force: true });
-  cy.get(".hmenu-item", { timeout: 2000 }).contains(menuOption).click();
+  cy.get("#hmenu-content>ul>li>a.hmenu-item", { timeout: 5000 })
+    .contains(menuOption)
+    .click();
   cy.contains(menuOption).should("be.visible");
 });
